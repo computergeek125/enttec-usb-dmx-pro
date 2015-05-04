08f6d787af52f3a12661bb6cf1f8eedfe58baaa0
Ryan Smith <ryansmith125@gmail.com>
2015-05-04 01:51:16 -0500

    Added widget tester On branch master Changes to be committed: 	new file:   enttectest_widget.py

---
+++ enttectest_widget.py
@@ -0,0 +1,34 @@
+import platform
+import sys
+import time
+import EnttecUsbDmxProWidget
+
+dport = -1
+
+if len(sys.argv) < 2:
+	if platform.system() == 'Linux':
+		dport = '/dev/ttyUSB0'
+	elif platform.system() == 'Mac':
+		dport = '/dev/tty.usbserial-ENT095626'
+else:
+	dport = sys.argv[1]
+dmx = EnttecUsbDmxProWidget.EnttecUsbDmxProWidget()
+if dport == -1:
+	dmx.list()
+	sys.stderr.write("ERROR: No serial port for DMX detected!\n")
+	sys.exit()
+dmx.setPort(dport)
+dmx.connect()
+
+def pulse():
+	while True:
+		for i in range(0,256):
+			dmx.sendDMX([i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i])
+			time.sleep(0.01)
+		for i in range(255,-1):
+			dmx.sendDMX([i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i])
+			time.sleep(0.01)
+
+def get():
+	while True:
+		print(dmx.getRecievedFrame())

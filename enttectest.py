ba7edea8788e79be6a1050166de76e9b07363471
Ryan Smith <ryansmith125@gmail.com>
2016-12-02 02:42:09 -0600

    Reorganized so that the functions are at the top and added blocking get new frame test

--- enttectest.py
+++ enttectest.py
@@ -1,24 +1,9 @@
+import os
 import platform
 import sys
 import time
 import EnttecUsbDmxPro
-
-dport = -1
-
-if len(sys.argv) < 2:
-	if platform.system() == 'Linux':
-		dport = '/dev/ttyUSB0'
-	elif platform.system() == 'Mac':
-		dport = '/dev/tty.usbserial-ENT095626'
-else:
-	dport = sys.argv[1]
-dmx = EnttecUsbDmxPro.EnttecUsbDmxPro()
-if dport == -1:
-	dmx.list()
-	sys.stderr.write("ERROR: No serial port for DMX detected!\n")
-	sys.exit()
-dmx.setPort(dport)
-dmx.connect()
+import colorama
 
 def pulse():
 	while True:
@@ -31,4 +16,56 @@ def pulse():
 
 def get():
 	while True:
-		print(dmx.getRecievedFrame())
+		print(dmx.getReceivedFrame())
+
+def getNext():
+	dmx.setDmxOnChange(enabled=True)
+	while True:
+		f = dmx.getNextFrame()["frame"]
+		#dmxprint([f[i] for i in [331,383,393,437,110,435,330,328,387,395,385,391,397,98,445,438,434,446]])
+		dmxprint(dmx.getNextFrame()["frame"])
+
+def filterList(inlist, string, inverse=False):
+	l = []
+	for i in range(len(inlist)-1,-1,-1):
+		if string in inlist[i] and not inverse:
+			l.append(inlist[i])
+	return l
+
+def dmxprint(data):
+	sys.stdout.write("{0}: ".format(len(data)))
+	if len(data) == 0:
+		sys.stdout.write("[]\n")
+		return
+	sys.stdout.write("[")
+	for i in data:
+		if i < 10:
+			sys.stdout.write("  {0}".format(i))
+		elif i < 100:
+			sys.stdout.write(" {0}".format(i))
+		else:
+			sys.stdout.write("{0}".format(i))
+		sys.stdout.write(", ")
+	sys.stdout.write("\b\b")
+	sys.stdout.write("]\n")
+
+colorama.init(autoreset=True)
+dport = -1
+
+if len(sys.argv) < 2:
+	if platform.system() == 'Linux':
+		dport = '/dev/ttyUSB0'
+	elif platform.system() == 'Darwin':
+		#dport = '/dev/tty.usbserial-ENT095626'
+		#dport = '/dev/tty.usbserial-EN175330'
+		dport = "/dev/" + filterList(filterList(os.listdir("/dev/"), "tty."), "-EN")[0]
+		print(dport)
+else:
+	dport = sys.argv[1]
+dmx = EnttecUsbDmxPro.EnttecUsbDmxPro()
+if dport == -1:
+	dmx.list()
+	sys.stderr.write("ERROR: No serial port for DMX detected!\n")
+	sys.exit()
+dmx.setPort(dport)
+dmx.connect()
